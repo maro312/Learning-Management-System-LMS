c66e2c3b1c6da4dc611daefbfad2c9c5e3c5b7d5
maro312 <marawan.ah.ab@gmail.com>
2024-12-16 17:10:25 +0200

    create project

---
+++ src/main/java/org/example/learningmanagementsystemlms/LearningManagementSystemLmsApplication.java
@@ -0,0 +1,15 @@
+package org.example.learningmanagementsystemlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningManagementSystemLmsApplication {
+
+	public static void main(String[] args) {
+
+//		SpringApplication.run(LearningManagementSystemLmsApplication.class, args);
+		System.out.println("why?");
+	}
+
+}
---
+++ src/test/java/org/example/learningmanagementsystemlms/LearningManagementSystemLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.learningmanagementsystemlms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningManagementSystemLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
